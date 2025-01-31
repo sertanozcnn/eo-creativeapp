f85560ebb562130924d1274c08a3df73b5d05d1f
Sertan <sertanozcan.tr@gmail.com>
2025-01-31 23:46:51 +0300

    Build and update production assets

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,23 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Pricing from "../pages/Pricing/Pricing";
 import Home from "../pages/Home/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/fiyatlandirma",
-        element: <Pricing />,
-      },
-    ],
-  },
-]);
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/fiyatlandirma" element={<Pricing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-export default router;
+export default Router;
