c3b4297ca1c144d679118afb4d1d043dc4de6ab4
Sertan <sertanozcan.tr@gmail.com>
2025-02-01 01:48:17 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Router from "./router/router";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Router />
+    <RouterProvider router={router} />
   </Provider>
 );
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Pricing from "../pages/Pricing/Pricing";
 import Home from "../pages/Home/Home";
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/fiyatlandirma" element={<Pricing />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/fiyatlandirma",
+        element: <Pricing />,
+      },
+    ],
+  },
+]);
 
-export default Router;
+export default router;
